1f26a3b1c55b6d1481c1f9064c6819f000c185b3
Konstantinos_Lykoudis <konstantinoslyk@gmail.com>
2024-03-13 19:14:52 +0200

    create constructor

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,54 @@
-public class TicTacToe {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel label = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    public TicTacToe() {
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.setTitle("Tic-Tac-Toe");
+        frame.getContentPane().setBackground(Color.WHITE);
+        frame.setLayout(new BorderLayout());
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        label.setBackground(Color.WHITE);
+        label.setForeground(Color.BLACK);
+        label.setFont(new Font("Helvetica", Font.BOLD, 75));
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setText("Tic-Tac-Toe");
+        label.setOpaque(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void oWins(int a, int b, int c){
+
+    }
 }
